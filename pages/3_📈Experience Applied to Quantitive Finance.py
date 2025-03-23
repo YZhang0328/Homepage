a6b68117bb49c61af32f72b3fc2b057ca4ab5d84
YZhang0328 <yujia.zhang.uom@gmail.com>
2025-03-23 22:21:26 +0000

    Changed experience description

--- pages/3_📈Experience Applied to Quantitive Finance.py
+++ pages/3_📈Experience Applied to Quantitive Finance.py
@@ -3,12 +3,71 @@ import streamlit as st
 with st.container():
 
     st.markdown(
-        '<p style="font-size: 18px;"> Modern trading and asset allocation increasingly relies on scientific and quantitative methodologies, rather than traditional experience-based approaches.  </p>',
-        unsafe_allow_html=True
-    )
-    st.markdown(
-        '<p style="font-size: 18px;"> In trading and asset allocation, data-based forecasting and mathematical formulation are pivotal, offering a structured and efficient way to predict the future price trend assisting the decision-making progress, optimize portfolios and manage risks while adhering to regulatory requirements. Developing effective investment strategies requires analyzing financial data, making prediction, formulating optimization problems to balance competing objectives—maximizing returns and minimizing risk within specified constraints.  </p>',
+        """
+        <p style="font-size: 20px;"> 
+            Modern trading and asset allocation increasingly relies on scientific and quantitative methodologies, 
+            rather than traditional experience-based approaches. 
+        </p>
+        
+        <p style="font-size: 18px;"> 
+            In trading and asset allocation, data-based forecasting and mathematical formulation are pivotal, 
+            offering a structured and efficient way to predict the future price trend assisting the decision-making process, 
+            optimize portfolios, and manage risks while adhering to regulatory requirements. Developing effective investment strategies 
+            requires analyzing financial data, making predictions, formulating optimization problems to balance competing objectives— 
+            maximizing returns and minimizing risk within specified constraints.
+        </p>
+        
+        <p style="font-size: 20px;"> 
+            My background provides expertise in two core areas essential to this process, as outlined below.
+        </p>
+
+        <p style="font-size: 20px;">
+            1. Financial data analysis and preparation:
+        </p>
+
+        <p style="font-size: 18px;">
+            1) I have strong analytical skills, which enable me to extract relevant insights from complex datasets 
+            and identify their significance to specific tasks. Excel and SQL are handy tools for me.
+        </p>
+
+        <p style="font-size: 18px;">
+            2) I have advanced programming proficiency in Python, MATLAB, AWS, and Git (5+ years Python experience, 
+            familiar with object-oriented programming), which allow me to process and model data efficiently. 
+            I have a strong commitment to test-driven development, unit tests, contract tests, integration tests, behaviour tests, etc.
+        </p>
+
+        <p style="font-size: 18px;">
+            3) A commitment to continuous learning has led me to actively pursue the CFA designation and deepen my understanding of finance.
+        </p>
+
+        <p style="font-size: 18px;">
+            4) My current role in a modeling and development team has sharpened my ability to approach challenges methodically and find the best solutions. 
+            I am always sensible about what I am doing and how to do it effectively.
+        </p>
+
+        <p style="font-size: 20px;">
+            2. Price forecasting and mathematical optimization for finance decisions:
+        </p>
+
+        <p style="font-size: 18px;">
+            1) I have strong expertise in time-series algorithm design, having developed many mature linear and nonlinear prediction models. 
+            These models, strategically designed and not publicly available, leverage fundamental linear regression techniques for forecasting 
+            and incorporate machine learning strategies for nonlinear data prediction, achieving superior accuracy.
+        </p>
+
+        <p style="font-size: 18px;">
+            2) With a Doctorate in optimization theory and applications from the UK, I bring a deep understanding of advanced optimization techniques.
+            I have knowledge in framing return expectations as objectives to maximize and incorporating risk—via variance/covariance, 
+            or deviation—either as constraints or factors to minimize.
+        </p>
+
+        <p style="font-size: 18px;">
+            3) My expertise extends to translating complex client requirements such as regulatory requirements into optimization frameworks, 
+            ensuring compliance while achieving portfolio objectives.
+        </p>
+        """,
         unsafe_allow_html=True
     )
 
- 
+
+	
